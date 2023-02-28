a784b6339e4b252e6518a0e8e31ea9e64a16a977
Artyom <artyombagaev150@yandex.ru>
2023-03-01 02:46:45 +0300

    Добавили комментарии к коду

--- Task__1/Program.cs
+++ Task__1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше либо равна 3.
 
-string[] GetArray(int size)
+string[] GetArray(int size)                             // Создаем массив. Количество строк задает пользователь.
 {
     string[] array = new string[size]; 
     for (int i=0; i<array.Length; i++)
@@ -12,7 +12,7 @@ string[] GetArray(int size)
     return array;
 }
 
-int CountUpper3(string[] inArray)
+int CountUpper3(string[] inArray)                      // Считаем количество строк больше или равное 3.
 {
     int count = 0;
     for (int i=0; i<inArray.Length; i++)
@@ -22,7 +22,7 @@ int CountUpper3(string[] inArray)
     return count;
 }
 
-string[] Str3Symbol(string[] inArray, int count)
+string[] Str3Symbol(string[] inArray, int count)       // Создаем новый массив и кладем в него значения >=3.
 {
     int j=0;
     string[] result = new string[count];
@@ -41,9 +41,9 @@ Console.Clear();
 Console.WriteLine("Введите количество строк: ");
 int countNumbers = int.Parse(Console.ReadLine()!);
 string[] newArray = GetArray(countNumbers);
-Console.WriteLine("Все введенные строки:");
-Console.WriteLine(String.Join(", ", newArray));
+Console.WriteLine("Все введенные строки:");            
+Console.WriteLine(String.Join(", ", newArray));          // Для наглядности выводим все строки.
 int count = CountUpper3(newArray);
 Console.WriteLine("Строки более 3х символов");
 string[] result = Str3Symbol(newArray, count);
-Console.WriteLine(String.Join(", ", result)); 
+Console.WriteLine(String.Join(", ", result));           // Выводим строки более 3х символов.
